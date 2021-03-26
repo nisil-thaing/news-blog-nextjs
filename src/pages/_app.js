e493511c3338ec2559096ea660375a9b1a288325
Thai (Isaac) Nguyen <isaac.ntthai.it@gmail.com>
2021-03-27 00:41:08 +0700

    Add Styled Component library and its basic configurations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,8 @@
+import GlobalStyles from 'styles/index.style';
+
 function MyApp ({ Component, pageProps }) {
   return <>
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-      
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-      
-      * {
-        box-sizing: border-box;
-      }      
-    `}</style>
+    <GlobalStyles />
     <Component { ...pageProps } />
   </>;
 }
