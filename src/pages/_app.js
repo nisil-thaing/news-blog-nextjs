8dc37d4a253ee3b9ae2def007b786b37912e949f
Thai (Nisil) Nguyen <isaac.ntthai.it@gmail.com>
2021-03-26 21:00:21 +0700

    Move "pages" and "styles" folders into "src" folder. Add jsconfig.json file

---
+++ src/pages/_app.js
@@ -0,0 +1,25 @@
+function MyApp ({ Component, pageProps }) {
+  return <>
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+      
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+      
+      * {
+        box-sizing: border-box;
+      }      
+    `}</style>
+    <Component { ...pageProps } />
+  </>;
+}
+
+export default MyApp
---
+++ src/pages/about-us.js
@@ -0,0 +1,5 @@
+function AboutUsPage () {
+  return <div>This is AboutUsPage</div>;
+}
+
+export default AboutUsPage;
