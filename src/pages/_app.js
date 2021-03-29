4e204332207af1244bb8fc45904272127f365237
Thai (Isaac) Nguyen <isaac.ntthai.it@gmail.com>
2021-03-29 21:37:10 +0700

    Fix bootstrap's importation still not working on SSR mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
 import GlobalStyles from 'styles/index.style';
 
 function MyApp ({ Component, pageProps }) {
   return <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        crossOrigin="anonymous"></link>
+    </Head>
     <GlobalStyles />
     <Component { ...pageProps } />
   </>;
