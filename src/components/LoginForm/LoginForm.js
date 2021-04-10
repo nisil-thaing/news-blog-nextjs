64fd1b4a26e33b7f09b438b30c80286c88180123
Thai (Nisil) Nguyen <isaac.ntthai.it@gmail.com>
2021-04-10 16:26:37 +0700

    Update dummy LoginForm component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import { Container } from './LoginForm.style';
+
 function LoginForm () {
-  return <>This is LoginForm</>;
+  return <Container>
+    <h1 className="mb-4 text-center">Log in</h1>
+    <div className="row">
+      <div className="col-6 pr-1">
+        <button
+          type="button"
+          className="d-flex justify-content-center align-items-center w-100 btn btn-primary">
+          <i className="mr-2 bi bi-facebook" />
+          Facebook
+        </button>
+      </div>
+      <div className="col-6 pl-1">
+        <button
+          type="button"
+          className="d-flex justify-content-center align-items-center w-100 btn btn-outline-secondary">
+          <i className="mr-2 bi bi-google" />
+          Google
+        </button>
+      </div>
+      <div className="col-12 mt-3">
+        <button
+          type="button"
+          className="d-flex justify-content-center align-items-center w-100 btn btn-link">
+          <i className="mr-2 bi bi-envelope" />
+          Or log in using email
+          <i className="ml-2 bi bi-arrow-down" />
+        </button>
+      </div>
+    </div>
+  </Container>;
 }
 
 export default LoginForm;
