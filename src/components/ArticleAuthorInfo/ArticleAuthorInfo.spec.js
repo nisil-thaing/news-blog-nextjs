a3e649f6193f0b0f242ae33ea410af7150cd60c2
Thai (Isaac) Nguyen <isaac.ntthai.it@gmail.com>
2021-05-19 18:58:48 +0700

    Add basic jest configurations

---
+++ src/components/ArticleAuthorInfo/ArticleAuthorInfo.spec.js
@@ -0,0 +1,24 @@
+// Link.react.test.js
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import ArticleAuthorInfo from './ArticleAuthorInfo';
+
+const SAMPLE_DATA = {
+  id: 'this_is_a_sample_id',
+  avatarUrl: 'https://alyadarinariestyadi46.files.wordpress.com/2015/02/motohira-hata-stand-by-me.jpg',
+  slug: 'this-is-a-sample-slug',
+  displayName: 'Motohiro Hata'
+};
+const UPDATED_AT = (new Date()).toUTCString();
+
+test('ArticleAuthorInfo shows right results while received full props', () => {
+  const component = renderer.create(
+    <ArticleAuthorInfo
+      data={ SAMPLE_DATA }
+      updatedAt={ UPDATED_AT }
+      timeToReadInMinutes={ 10 } />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
